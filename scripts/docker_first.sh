1fa6d2f6c7de01cfe67d1254ffb67dda52bd5b91
root <root@server-131042-020521.tino>
2022-04-14 22:37:24 +0530

    Fix CI & mount data FE to nginx

--- scripts/docker_first.sh
+++ scripts/docker_first.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker network create itconnect_nginx
+networkName=itconnect_nginx
+volumeFEBuilt=itconnect_fe_built
+
+if [[ "$(docker network ls | grep "${networkName}")" == "" ]] ; then
+  docker network create "${networkName}"
+fi
+
+if [[ "$(docker volume ls | grep "${volumeFEBuilt}")" == "" ]] ; then
+  docker volume create "${volumeFEBuilt}"
+fi
