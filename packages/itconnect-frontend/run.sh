1f8684b0d488c838ea5fdcf82a35c31ab5486d8e
root <root@server-131042-020521.tino>
2022-06-30 20:09:56 +0530

    [CI] fix

--- packages/itconnect-frontend/run.sh
+++ packages/itconnect-frontend/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -x #echo on
+
 source ../../scripts/docker_first.sh
 
 git pull
 
-docker rmi $(docker images -f "dangling=true" -q)
+docker rmi $(docker images -f "dangling=true" -q) -f
 docker-compose stop
 docker-compose rm -f
 docker rmi wawahuy/itconnect_frontend:latest
