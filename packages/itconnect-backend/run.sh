94876ba64b0c7f2bf66631163ceaa574bfed32dc
Gia Huy <nguyengiahuy16@gmail.com>
2022-06-30 21:59:49 +0700

    [CI] [BE] remove up to dockerhub

--- packages/itconnect-backend/run.sh
+++ packages/itconnect-backend/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -x #echo on
 
 source ../../scripts/docker_first.sh
 
-docker rmi $(docker images -f "dangling=true" -q)
+git pull
+
+docker rmi $(docker images -f "dangling=true" -q) -f
 docker-compose stop
 docker-compose rm -f
 docker rmi wawahuy/itconnect_backend:latest
